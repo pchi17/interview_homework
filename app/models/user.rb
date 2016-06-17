82402cd35e9d016c45735447ef053f76a710055d
pchi17 <pengfei.p.chi@gmail.com>
2016-06-17 14:17:53 -0700

    add spec for User validations, remove turbolinks, us js instead of coffee

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   enum role: [:teacher, :student]
 
   validates :username, presence: true, uniqueness: true
-  validates :role, presence: true
+  validates :role,     presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+  describe 'validations' do
+    it { expect(subject).to validate_presence_of(:username) }
+    it { expect(subject).to validate_presence_of(:role) }
+    it { expect(subject).to define_enum_for(:role).with([:teacher, :student]) }
+  end
 end
