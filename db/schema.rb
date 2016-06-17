c6754a61ca15803eea8646f2c80b342e176b6791
pchi17 <pengfei.p.chi@gmail.com>
2016-06-17 13:05:34 -0700

    switch and config database to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150529213510) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "username",   null: false
     t.integer  "role",       null: false
@@ -20,6 +23,6 @@ ActiveRecord::Schema.define(version: 20150529213510) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
