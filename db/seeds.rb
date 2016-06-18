bf5ef293a5480508ead445db84c639bed162032c
pchi17 <pengfei.p.chi@gmail.com>
2016-06-17 17:46:27 -0700

    seed db with users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-FactoryGirl.create(:teacher, username: 'teacher')
-FactoryGirl.create(:student, username: 'student')
+# create 2 teachers
+FactoryGirl.create(:teacher, username: 'philip')
+FactoryGirl.create(:teacher, username: 'mike')
+
+
+50.times do |i|
+  FactoryGirl.create(:student, username: "student_#{i}")
+end
