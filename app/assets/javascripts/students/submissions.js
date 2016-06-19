be1c2f124c07c1db3e63563ea2b980138431afb9
pchi17 <pengfei.p.chi@gmail.com>
2016-06-19 09:11:35 -0700

    add feature to create new submission via ajax

--- app/assets/javascripts/students/submissions.js
+++ app/assets/javascripts/students/submissions.js
@@ -1,2 +1,47 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+  var $newSubmissionFormCol = $('.new-submission-form-col');
+  
+  $('.new-submission-btn').click(function(e) {
+    e.preventDefault();
+    
+    $('.submissions-row').hide();
+    $newSubmissionFormCol.show();
+    $newSubmissionFormCol.find('textarea').focus();
+    $(this).hide();
+    $('.show-submissions-btn').show();
+  })
+  
+  $('.show-submissions-btn').click(function() {
+    $('.submissions-row').show();
+    $newSubmissionFormCol.hide();
+    $(this).hide();
+    $('.new-submission-btn').show();
+  })
+  
+  $newSubmissionFormCol.on('submit', '#new_submission', function(e) {
+    e.preventDefault();
+    var path = $(this).attr('action');
+    var data = $(this).serialize();
+    $.post(path, data)
+      .done(function(response) {
+        $('.submissions-row').show();
+        $newSubmissionFormCol.hide();
+        $newSubmissionFormCol.find('.alert').remove();
+        $('.show-submissions-btn').hide();
+        $('.new-submission-btn').show();
+        $('.submissions-tr').last().fadeOut(function() {
+          $(this).remove();
+          $('.submissions-tbody').prepend(response);
+          $('.submissions-tr').first().fadeOut().fadeIn();
+        })
+      })
+      .fail(function(error) {
+        $newSubmissionFormCol.empty();
+        $newSubmissionFormCol.html(error.responseText);
+        $newSubmissionFormCol.find('textarea').focus();
+      })
+  })
+})
--- app/controllers/students/submissions_controller.rb
+++ app/controllers/students/submissions_controller.rb
@@ -3,6 +3,7 @@ class Students::SubmissionsController < StudentsController
   def index
     @submissions = @assignment.submissions.order(created_at: :desc)
       .paginate(page: params[:page], per_page: 5)
+    @submission  = @assignment.submissions.build
   end
   
   def new
@@ -12,10 +13,18 @@ class Students::SubmissionsController < StudentsController
   def create
     @submission = @assignment.submissions.build(submission_params)
     if @submission.save
-      flash[:success] = 'Answer Submitted!'
-      redirect_to students_assignment_submissions_path(@assignment)
+      if request.xhr?
+        render partial: 'students/submissions/submission', locals: { submission: @submission }
+      else
+        flash[:success] = 'Answer Submitted!'
+        redirect_to students_assignment_submissions_path(@assignment)
+      end
     else
-      render :new
+      if request.xhr?
+        render partial: 'students/submissions/new_submission_form', status: :bad_request
+      else
+        render :new
+      end
     end
   end
   
