a693caba26258a80c719eb3ab16b9b2e560bc0ab
pchi17 <pengfei.p.chi@gmail.com>
2016-06-19 07:39:46 -0700

    style student part of app

--- app/helpers/students/submissions_helper.rb
+++ app/helpers/students/submissions_helper.rb
@@ -1,2 +1,11 @@
 module Students::SubmissionsHelper
+  def print_submission_time(submission_time, due_date)
+    if submission_time.to_date < due_date
+      klass = 'text-success'
+    else
+      klass = 'text-danger'
+    end
+    
+    content_tag :span, submission_time.strftime('%a, %Y %b %d %H:%M'), class: "#{klass} date"
+  end
 end
--- app/helpers/teachers/homeworks_helper.rb
+++ app/helpers/teachers/homeworks_helper.rb
@@ -1,2 +1,14 @@
 module Teachers::HomeworksHelper
+  def print_due_date(due_date)
+    klass = case
+    when due_date > 1.week.from_now
+      'text-success'
+    when due_date >= Date.today
+      'text-warning'
+    when due_date < Date.today
+      'text-danger'
+    end
+    
+    content_tag :span, due_date.strftime('%a, %b %d, %Y'), class: "#{klass} date"
+  end
 end
